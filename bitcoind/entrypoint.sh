3a8ded889d427bf9c9570b4e4902ccd570f34f2b
prakashr1984 <prakash.r1984@gmail.com>
2019-10-29 06:05:47 +0800

    updated docker image

---
+++ bitcoind/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+if [ $(echo "$1" | cut -c1) = "-" ]; then
+  echo "$0: assuming arguments for bitcoind"
+
+  set -- bitcoind "$@"
+fi
+
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
+   chmod 700 "$BITCOIN_DATA"
+   chown -R bitcoin "$BITCOIN_DATA"
+fi
+
+if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+  echo
+  exec gosu bitcoin "$@"
+fi
+
+echo
+exec "$@"
